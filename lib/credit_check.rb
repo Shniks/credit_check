eb029f5d3f0d84e603659c9b9984b6b88dcd7bf7
Lihkin <shniks@gmail.com>
2017-12-02 17:32:14 -0700

    Code amended to include Defined Methods.

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,22 +1,54 @@
-def credit_card
-  card_number = "342801633855673"
-  card_reverse = card_number.to_i.digits
-  sum = 0
-  count = 0
-
-  card_reverse.each do |num|
-    num *= 2 if count.odd?
-    num = 1 + (num - 10) if num >= 10 
-    sum += num
-    count += count
-  end
-
-  if sum % 10 == 0
-    puts "The credit card number, #{card_number}, is valid! "
-  else
-    puts "The credit card number, #{card_number}, is Invalid !"
-  end
-
+def credit_card_input(number)
+  number.to_i.digits
 end
 
-credit_card
+def doubles_number(number)
+  count = 0
+  credit_card_input(number).map do |num|
+    count += 1
+    if count.even?
+      num *=2
+    else
+      num
+    end
+  end
+end
+
+def more_than_10(number)
+  doubles_number(number).map do |num|
+    if num >= 10
+      num = 1 + (num - 10) if num >= 10
+    else
+      num
+    end
+  end
+end
+
+def validity_check(number)
+  sum = 0
+  more_than_10(number).map do |num|
+    sum += num
+  end
+  sum
+end
+
+def result_validity(number)
+    if validity_check(number) % 10 == 0
+    puts "The credit card number, #{number}, is valid! "
+    else
+    puts "The credit card number, #{number}, is Invalid !"
+    end
+end
+
+
+# Sample data follows:
+
+result_validity("342801633855673") # Invalid
+result_validity("5541808923795240") #Valid
+result_validity("4024007136512380") #Valid
+result_validity("6011797668867828") #Valid
+result_validity("5541801923795240") # Invalid
+result_validity("4024007106512380") # Invalid
+result_validity("6011797668868728") # Invalid
+result_validity("342804633855673") # Amex Valid
+result_validity("342801633855673") # Amex Invalid
