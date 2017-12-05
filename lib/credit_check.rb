c8e47c8469c82eb8e69bd08d6a16b7c277372133
Lihkin <shniks@gmail.com>
2017-12-05 12:55:23 -0700

    Updated to include a Class.

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,54 +1,62 @@
-def credit_card_input(card)
-  card.to_i.digits
-end
+class CreditCardCheck
 
-def doubles_number(doubles)
-  count = 0
-  credit_card_input(doubles).map do |num|
-    count += 1
-    if count.even?
-      num *=2
-    else
-      num
+  attr_reader :card_number
+
+  def initialize(card_number)
+   @card_number = card_number
+ end
+
+  def credit_card_input(card_number)
+    card_number.to_i.digits
+  end
+
+  def doubles_number(doubles)
+    count = 0
+    credit_card_input(doubles).map do |num|
+      count += 1
+      if count.even?
+        num *=2
+      else
+        num
+      end
     end
   end
-end
 
-def more_than_10(tenandabove)
-  doubles_number(tenandabove).map do |num|
-    if num >= 10
-      num = 1 + (num - 10)
-    else
-      num
+  def more_than_10(tenandabove)
+    doubles_number(tenandabove).map do |num|
+      if num >= 10
+        num = 1 + (num - 10)
+      else
+        num
+      end
     end
   end
-end
 
-def validity_check(validity)
-  sum = 0
-  more_than_10(validity).map do |num|
-    sum += num
-  end
-  sum
-end
-
-def result_validity(number)
-    if validity_check(number) % 10 == 0
-      puts "The credit card number, #{number}, is valid! "
-    else
-      puts "The credit card number, #{number}, is Invalid !"
+  def validity_check(validity)
+    sum = 0
+    more_than_10(validity).map do |num|
+      sum += num
     end
-end
+    sum
+  end
 
+  def result_validity
+      if validity_check(@card_number) % 10 == 0
+        puts "The credit card number, #{@card_number}, is valid! "
+      else
+        puts "The credit card number, #{@card_number}, is Invalid!"
+      end
+  end
+end
 
 # Sample data follows:
 
-result_validity("342801633855673") # Invalid
-result_validity("5541808923795240") #Valid
-result_validity("4024007136512380") #Valid
-result_validity("6011797668867828") #Valid
-result_validity("5541801923795240") # Invalid
-result_validity("4024007106512380") # Invalid
-result_validity("6011797668868728") # Invalid
-result_validity("342804633855673") # Amex Valid
-result_validity("342801633855673") # Amex Invalid
+CreditCardCheck.new("342801633855673").result_validity # Invalid
+CreditCardCheck.new("5541808923795240").result_validity #Valid
+CreditCardCheck.new("4024007136512380").result_validity #Valid
+CreditCardCheck.new("6011797668867828").result_validity #Valid
+CreditCardCheck.new("5541801923795240").result_validity # Invalid
+CreditCardCheck.new("4024007106512380").result_validity # Invalid
+CreditCardCheck.new("6011797668868728").result_validity # Invalid
+CreditCardCheck.new("342804633855673").result_validity # Amex Valid
+CreditCardCheck.new("342801633855673").result_validity # Amex Invalid
